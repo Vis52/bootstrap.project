4d2738f8a72110c3563034f2274ffb3a188b5c95
vis52 <vs6773817@gmail.com>
2025-03-03 18:47:37 +0530

    ,

--- script.js
+++ script.js
@@ -7,3 +7,30 @@ console.log(response);
 let data = await response.json();
 console.log(data);
 };
+
+let c = 10;
+let b = 17;
+ let a = c + b;
+console.log(a)
+
+
+const d = 120;
+let s = "vishal";
+z = s+d;
+console.log(z)
+console.log(typeof z , typeof a)
+
+let age = 21;
+let name = "shal";
+if (age>=18){
+    if(name="vishal"){
+        console.log("vishal");
+    }
+    else{
+        console.log("name not match");
+    }
+    console.log("you are eligible");
+
+}else{
+    console.log("sorry! you are not eligible");
+}
