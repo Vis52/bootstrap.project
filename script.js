199b17f652e0f5f3f8518852966ed6ff7921ce6c
vis52 <vs6773817@gmail.com>
2025-03-04 18:40:13 +0530

    ,

--- script.js
+++ script.js
@@ -1,36 +1,42 @@
-const URL = "https://api.webscraping.ai/html?url=https://example.com&api_key=your_api_key_here";
+// const URL = "https://api.webscraping.ai/html?url=https://example.com&api_key=your_api_key_here";
 
-const getFacts = async ()=>{
-    console.log("gettingdata...");
-let response = await fetch(URL);
-console.log(response);
-let data = await response.json();
-console.log(data);
-};
+// const getFacts = async ()=>{
+//     console.log("gettingdata...");
+// let response = await fetch(URL);
+// console.log(response);
+// let data = await response.json();
+// console.log(data);
+// };
 
-let c = 10;
-let b = 17;
- let a = c + b;
-console.log(a)
+// let c = 10;
+// let b = 17;
+//  let a = c + b;
+// console.log(a)
 
 
-const d = 120;
-let s = "vishal";
-z = s+d;
-console.log(z)
-console.log(typeof z , typeof a)
+// const d = 120;
+// let s = "vishal";
+// z = s+d;
+// console.log(z)
+// console.log(typeof z , typeof a)
 
-let age = 21;
-let name = "shal";
-if (age>=18){
-    if(name="vishal"){
-        console.log("vishal");
-    }
-    else{
-        console.log("name not match");
-    }
-    console.log("you are eligible");
+// let age = 21;
+// let name = "shal";
+// if (age>=18){
+//     if(name="vishal"){
+//         console.log("vishal");
+//     }
+//     else{
+//         console.log("name not match");
+//     }
+//     console.log("you are eligible");
 
-}else{
-    console.log("sorry! you are not eligible");
+// }else{
+//     console.log("sorry! you are not eligible");
+// }
+
+
+
+for (i=1; i<=10; i++){
+    console.log("18 x" +i+ "=" +18*i) 
 }
